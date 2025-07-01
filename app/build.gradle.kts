2761406a1a18a8bc8f04fee5e1e5036b81e332a5
Meron T. Gebru <meronteklehaymanotgebru@gmail.com>
2025-07-01 21:59:45 +0300

    Initial boilerplate setup with compose and project structure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,6 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation("androidx.navigation:navigation-compose:2.9.0")
-    implementation("androidx.compose.material:material-icons-extended:1.7.8")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
